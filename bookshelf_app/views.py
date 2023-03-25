362c044994c05cb0740c21ee3b78e34394162382
baptistapaulo <paulo.a.baptista@gmail.com>
2023-03-25 21:57:29 +0000

    Render data on to home html template

--- bookshelf_app/views.py
+++ bookshelf_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def get_bookshelf_app(request):
-    return render(request, 'bookshelf_app/home.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'bookshelf_app/home.html', context)
