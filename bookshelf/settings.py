6697b8418e2033ad6c55fcb47b97fbd162d943be
baptistapaulo <paulo.a.baptista@gmail.com>
2023-03-28 22:36:35 +0000

    Final change to settings.py with debug disabled and allowed hosts setup for Heroku url

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v-+0te$tvdxzrz19t!8&ea#ws6fem0pc&r3brcxa(qvdu13f0a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [pp4-bookshelf.herokuapp.com]
 
 
 # Application definition
